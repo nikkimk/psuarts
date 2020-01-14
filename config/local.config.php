140faaaf6544bbc88c310ad904d3e66adc143aa9
Nikki Massaro Kauffman <nikkimk06@gmail.com>
2020-01-14 08:39:55 -0500

    init psuarts

---
+++ config/local.config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'view_manager' => [
+        'template_map' => [
+            'layout/layout' => __DIR__ . '/../themes/psuarts/view/layout/layout.phtml',
+            'omeka/index/index' => __DIR__ . '/../themes/psuarts/view/omeka/index/index.phtml',
+        ],
+    ],
+];
